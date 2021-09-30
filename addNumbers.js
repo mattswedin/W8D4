7467f575b731d0b090c98555e559676f0b5f930e
Matthew Swedin <matthew.swedin@gmail.comgit config --global user.email matthew.swedin@gmail.com>
2021-09-30 17:21:53 -0400

    addNumbers complete

--- addNumbers.js
+++ addNumbers.js
@@ -1,6 +1,27 @@
-
+// const { SlowBuffer } = require('buffer');
+let readline = require('readline')
 
 let reader = readline.createInterface( {
     input: process.stdin,
-    output: process.stout
+    output: process.stdout
 });
+
+function addNumbers(sum, numsLeft, completionCallback) {
+
+    // if (numsLeft === 0) {
+    //     return completionCallback(sum);
+    // }
+
+    if (numsLeft > 0) {
+        reader.question('Please input a number!', function (numStr) {
+            const thisNumber = parseInt(numStr);
+            sum += thisNumber;
+            console.log("Partial sum: " + sum);
+            addNumbers(sum, numsLeft - 1, completionCallback);
+        })
+
+    } else {completionCallback(sum)}
+    // return addNumbers(sum, numsLeft, completionCallback);
+};
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
